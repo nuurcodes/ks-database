d6a66f812e973a77cb29893357a33f61147e5806
NinjaNuur <71561866+nuurcodes@users.noreply.github.com>
2022-04-28 12:08:38 +0100

    chore: rename function

--- src/Programmability/Functions/update_user_password.sql
+++ src/Programmability/Functions/update_user_password.sql
@@ -1,5 +1,5 @@
 create
-    or replace function change_user_password(current_plain_password varchar, new_plain_password varchar)
+    or replace function update_user_password(current_plain_password varchar, new_plain_password varchar)
     returns json
     language plpgsql
     security definer
